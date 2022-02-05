65b1ced0aa23d54e6d395d90bb4b5e088aac1529
Felix De Montis <felix.demontis@orf.at>
2022-02-05 19:49:13 +0100

    Initial Commit

---
+++ LottieExperiment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LottieExperiment
+//
+//  Created by Felix De Montis on 05.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LottieExperiment/LottieExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieExperimentApp.swift
+//  LottieExperiment
+//
+//  Created by Felix De Montis on 05.02.22.
+//
+
+import SwiftUI
+
+@main
+struct LottieExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
